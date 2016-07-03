9eef9d9111a34eff265b07c52064ce5e28e880b8
pablomm <pablo.mark@hotmail.com>
2016-07-03 15:23:39 +0200

    Add verbose class

---
+++ tweetbot/utils.py
@@ -0,0 +1,23 @@
+
+from __future__ import print_function
+
+
+class Verbose:
+	def __init__(self, verbose_level=0):
+		self.verbose_level = verbose_level
+
+	def lprint(self, level, line):
+		if self.verbose_level <= level:
+			print(line)
+
+	def vprint(self, line):
+		self.lprint(1, line)
+
+	def vvprint(self, line):
+		self.lprint(2, line)
+
+
+class QueuedInteraction:
+
+	def __init__(self, api=None, **options):
+		pass
