eca7ed4b2137e875493a3b4c36105047bb040b23
pablomm <pablo.mark@hotmail.com>
2016-07-05 18:34:13 +0200

    Listener instance out of the loop, thread stopped with ^C and giveaway lists are now public

--- giveAway.py
+++ giveAway.py
@@ -10,24 +10,29 @@ access_token= ""
 access_token_secret= ""
 
 #Bot lists
-track_list = []
-ignore_list = []
-follow_list = []
-fav_list = []
+track_list = ["retweet to win","sorteo RT","concurso RT"]
+ignore_list = ["plz","ayuda","gracias","please","favor","signup","thanks","justin","bieber","5sos","vma","minecraft","vote","vota","twitch"]
+follow_list = ["#follow","follow","sigue","sigueme","seguir","following","siguiendo","seguidores","seguidor","rt+follow"]
+fav_list = ["fav","rt+fav","fave","favorito","favorite"]
 
-auth = OAuthHandler(consumer_key, consumer_secret) #autentificacion para streaming
+auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = API(auth)
 
+listener = GiveawayBot(api, follow_list, fav_list, ignore_list, verbose_level=2)
 
 if __name__ == '__main__':
 
 	while True:
 		try:
-			stream = Stream(auth, GiveawayBot(api, follow_list, fav_list, ignore_list, verbose_level=2))
+			stream = Stream(auth, listener)
 			stream.filter(track = track_list)
 
 		except KeyboardInterrupt, k:
 			if("y" in raw_input("Are you sure?")):
+				listener.stop()
 				exit()
+
+		except UnicodeEncodeError:
+			print(">> Unicode exception")
 
