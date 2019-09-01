387d9fe71ba2b6409f141637cc8e7b8b46d1ed56
Pablo Marcos <pablo.marcosm@protonmail.com>
2019-09-01 13:42:35 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -52,7 +52,10 @@ setup(
         "Environment :: Console",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Natural Language :: English",
-        "Programming Language :: Python :: 3.6",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        "Programming Language :: Python :: 3.7",
     ],
     entry_points = {
         'console_scripts': ['btweet=btweet:main'],
